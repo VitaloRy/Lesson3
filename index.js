3b1ecd6e34783324df66f3e1ca777d8e5581bece
Vitalik <vitalik.ryazantsev@gmail.com>
2025-12-18 10:58:26 +0300

    HomeWord

---
+++ index.js
@@ -0,0 +1,13 @@
+const pinCode = ["1111", "5678", "1234", "9999"];
+const myPin = pinCode[2];
+
+for (Pin of pinCode) {
+ if (Pin === "1234"){
+    console.log("Ваш пин код")
+break;
+}
+console.log ("Неверный пинкод"+ Pin );
+
+}
+
+  
